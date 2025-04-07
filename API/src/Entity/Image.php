5e2ff30a067d3d35f340de643cf2e0d3a6c87ea9
ZakariaLiveCampus <zakaria.saber@livecampus.me>
2025-04-07 16:30:50 +0200

    Modification de la structure bdd

--- API/src/Entity/Image.php
+++ API/src/Entity/Image.php
@@ -4,30 +4,28 @@ namespace App\Entity;
 
 use App\Repository\ImageRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: ImageRepository::class)]
 class Image
 {
     #[ORM\Id]
-    #[ORM\GeneratedValue]
+    #[Assert\Uuid]
     #[ORM\Column]
-    private ?int $id = null;
+    private string $id;
 
     #[ORM\Column(length: 255)]
-    private ?string $name = null;
+    private string $name;
 
     #[ORM\Column(length: 255)]
-    private ?string $url = null;
+    private string $url;
 
-    #[ORM\Column]
-    private ?int $counter = null;
-
-    public function getId(): ?int
+    public function getId(): string
     {
         return $this->id;
     }
 
-    public function getName(): ?string
+    public function getName(): string
     {
         return $this->name;
     }
@@ -39,7 +37,7 @@ class Image
         return $this;
     }
 
-    public function getUrl(): ?string
+    public function getUrl(): string
     {
         return $this->url;
     }
@@ -47,18 +45,6 @@ class Image
     public function setUrl(string $url): static
     {
         $this->url = $url;
-
-        return $this;
-    }
-
-    public function getCounter(): ?int
-    {
-        return $this->counter;
-    }
-
-    public function setCounter(int $counter): static
-    {
-        $this->counter = $counter;
 
         return $this;
     }
